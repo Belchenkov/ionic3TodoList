35fa023a9a7c34537eba33296d94587470e3b081
Belchenkov <u608110@gmail.com>
2017-06-14 13:09:37 +0300

    Details Page & Delete Task

--- src/pages/todo-details/todo-details.ts
+++ src/pages/todo-details/todo-details.ts
@@ -1,24 +1,49 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { EditTodoPage } from '../edit-todo/edit-todo'; 
+import {TodosPage} from '../todos/todos';
 
-/**
- * Generated class for the TodoDetailsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 @IonicPage()
 @Component({
   selector: 'page-todo-details',
   templateUrl: 'todo-details.html',
 })
 export class TodoDetailsPage {
+  todo: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public storage: Storage, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TodoDetailsPage');
+    this.todo = {
+      id: this.navParams.get('id'),
+      text: this.navParams.get('text'),
+      body: this.navParams.get('body'),
+      due: this.navParams.get('due'),
+    };
+  }
+
+  onEditClick() {
+    
+  }
+
+  onDeleteClick() {
+    this.storage.ready().then(() => {
+      this.storage.get('todos').then((val) => {
+        let todos = JSON.parse(val);
+        for(let i = 0; i < todos.length; i++) {
+          if (todos[i].id == this.todo.id) {
+            todos.splice(i, 1);
+          }
+        }
+
+        this.storage.set('todos', JSON.stringify(todos));
+        
+        this.navCtrl.push(TodosPage);
+      })
+    });
   }
 
 }
